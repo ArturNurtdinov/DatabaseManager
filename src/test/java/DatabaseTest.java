1b3585a06a4cf0b022366c170f69ef4060c47490
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-02-22 11:02:02 +0300

    Remove unnecessary import

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -3,7 +3,6 @@ import db.ProductDAO;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
