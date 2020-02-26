5168439c34f71110dbb2bd47c157d022fbcbefed
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-02-26 14:32:06 +0300

    Controller for main window

---
+++ src/main/java/application/controllers/MainWindowController.java
@@ -0,0 +1,13 @@
+package application.controllers;
+
+import db.Product;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+
+public class MainWindowController {
+    @FXML
+    TableView<Product> productTable;
+
+
+}
