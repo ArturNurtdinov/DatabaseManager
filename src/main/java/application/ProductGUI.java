b965882ff4475af6e7a5c3b9f534d84cf69d2185
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-02-28 23:29:15 +0300

    Primary stage handler

--- src/main/java/application/ProductGUI.java
+++ src/main/java/application/ProductGUI.java
@@ -1,14 +1,22 @@
 package application;
 
+import application.controllers.EnterWindowController;
+import application.controllers.LoginWindowController;
+import application.controllers.MainWindowController;
+import db.Product;
+import db.ProductDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ProductGUI extends Application {
+
+    Stage primaryStage;
+    ProductDAO dao;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,9 +24,69 @@ public class ProductGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
-        primaryStage.setTitle("Products Database");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        this.primaryStage = primaryStage;
+        showLoginWindow();
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public void showLoginWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/login_window.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.setTitle("Login");
+
+            LoginWindowController controller = loader.getController();
+            controller.provideApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMainWindow(int numberToGenerate) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/main_window.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.setTitle("Products");
+
+            MainWindowController controller = loader.getController();
+            controller.provideApp(this);
+            controller.provideDao(dao);
+            controller.provideNumberToGenerate(numberToGenerate);
+            dao.clear();
+            for (int i = 0; i < numberToGenerate; i++) {
+                dao.add(new Product(i, "product " + i, i * 10));
+                controller.getTable().getItems().add(new Product(i, "product " + i, i * 10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showEnterWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/enter_window.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.setTitle("Please enter amount of rows to generate");
+
+            EnterWindowController controller = loader.getController();
+            controller.provideApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showErrorWindow(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.initOwner(primaryStage);
+        alert.setTitle("Error");
+        alert.setHeaderText("Something went wrong. Please read information below.");
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+    public void provideDao(ProductDAO dao) {
+        this.dao = dao;
     }
 }
